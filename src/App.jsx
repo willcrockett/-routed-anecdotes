fc033c99c03d7bea9c2d6fa7c0dd2dac393e2880
William Crockett <willcrockett4@gmail.com>
2024-01-22 18:15:57 -0800

    finished 7.2

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,25 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      }</li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) =>  {
+  const id =  useParams().id
+  const anecdote = anecdotes.find(a => a.id == Number(id))
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -137,6 +152,7 @@ const App = () => {
         </div>
 
         <Routes>
+          <Route path='/anecdotes/:id' element={<Anecdote anecdotes={anecdotes}/>} />
           <Route path='/anecdotes' element={<AnecdoteList anecdotes={anecdotes}/>}  />
           <Route path='/createnew'  element={<CreateNew />} />
           <Route path='about' element={<About />} />
